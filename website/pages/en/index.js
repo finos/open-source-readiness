a671ea6e3261e99e331124fcbb2ec6ac213d561f
Aaron Williamson <aaron@copiesofcopies.org>
2021-11-02 18:00:53 -0400

    Update links in index.js

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -123,13 +123,13 @@ class Index extends React.Component {
             The OSR SIG is dedicated to helping financial services firms advance their open source programs. Here's what we have to offer:</p>
             <ul>
               <li>
-                <strong>Meetings</strong> of the SIG are held on the first Wednesday of each month at 10am US/Eastern, and are open to anyone who would like to participate. Meetings include group discussions of open source best practices, guest presentations from open source experts, and collaborative sessions to produce resources for the industry.
+                <strong><a href="/docs/meetings/">Meetings</a></strong> of the SIG are held on the first Wednesday of each month at 10am US/Eastern, and are open to anyone who would like to participate. Meetings include group discussions of open source best practices, guest presentations from open source experts, and collaborative sessions to produce resources for the industry.
               </li>
               <li>
-                <strong>Media</strong> from guest presentations and SIG sessions provides invaluable insights from experts and FSI colleagues on a range of topics related to open source usage, contribution, and compliance.
+                <strong><a href="/docs/media/">Media</a></strong> from guest presentations and SIG sessions provides invaluable insights from experts and FSI colleagues on a range of topics related to open source usage, contribution, and compliance.
               </li>
               <li>
-                We produce <strong>resources</strong> to support FSI firms in developing robust open source programs, including an <a href="https://github.com/finos/OSLC-handbook">Open Source License Compliance Handbook</a> with information about complying with common open source licenses, a <a href="https://www.finos.org/business-value-of-open-source-for-financial-services-firms-download-page?hsLang=en-us">executive whitepaper on the business value of open source</a>, and a <a href="https://github.com/finos/reference-foss-policy">reference open source policy</a> for FSI firms. 
+                We produce <strong><a href="/docs/resources/">resources</a></strong> to support FSI firms in developing robust open source programs, including an <a href="https://github.com/finos/OSLC-handbook">Open Source License Compliance Handbook</a> with information about complying with common open source licenses, a <a href="https://www.finos.org/business-value-of-open-source-for-financial-services-firms-download-page?hsLang=en-us">executive whitepaper on the business value of open source</a>, and a <a href="https://github.com/finos/reference-foss-policy">reference open source policy</a> for FSI firms. 
               </li>
             </ul>
           </div>
@@ -143,7 +143,7 @@ class Index extends React.Component {
               Subscribe to our <a href="https://groups.google.com/a/finos.org/forum/#!forum/osr-regulatory-compliance">mailing list</a> by sending an email to <a href="mailto:osr+subscribe@finos.org">osr+subscribe@finos.org</a>, and get updates on upcoming meetings and other activity.
               </li>
               <li>
-                Check out our <strong>open source projects</strong> on GitHub. All of our resources are collaboratively produced as open source projects, and we're always on the lookout for ways to enable participants to share knowledge and best practices in a form that's useful to their peers. If you have a project to propose, <a href="https://github.com/finos/open-source-readiness/issues">open a GitHub issue</a> and share your vision!
+                Check out our <strong><a href="/docs/projects/">open source projects</a></strong>. All of our resources are collaboratively produced as open source projects, and we're always on the lookout for ways to enable participants to share knowledge and best practices in a form that's useful to their peers. If you have a project to propose, <a href="https://github.com/finos/open-source-readiness/issues">open a GitHub issue</a> and share your vision!
               </li>
             </ul>
           </div>
