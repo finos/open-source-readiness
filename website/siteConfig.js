1d85087887a995fa701353fcca5096623fb95603
James McLeod <james.mcleod@finos.org>
2022-04-11 11:08:32 +0100

    update header link

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -27,7 +27,7 @@ const siteConfig = {
   // Header menu
   headerLinks: [
     {doc: 'meetings', label: 'MEETINGS'},
-    {doc: 'osr-references/resources', label: 'RESOURCES'},
+    {doc: 'osr-resources/resources', label: 'RESOURCES'},
     {doc: 'about', label: 'ABOUT'},
     {blog: false}
   ],
