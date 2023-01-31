5309944ef3b1274bd3750abc70f101196a235130
Rob Moffat <rob@kite9.com>
2023-01-31 11:40:08 +0000

    Removed old website

--- v2-website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/rob/Documents/finos/open-source-readiness/v2-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/rob/Documents/finos/open-source-readiness/v2-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/rob/Documents/finos/open-source-readiness/v2-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/rob/Documents/finos/open-source-readiness/v2-website/src/css/customTheme.css'),
-];
