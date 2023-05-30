661748df21692883d7267585a99f7d563bcc2057
Rob Moffat <rob@kite9.com>
2023-05-30 14:55:58 +0100

    Created react table of courses

--- website/src/theme/CourseList/index.js
+++ website/src/theme/CourseList/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import {usePluginData} from '@docusaurus/useGlobalData'
-import Link from '@docusaurus/Link';
 import {useLocation} from '@docusaurus/router';
+import PesterDataTable from './PesterDataTable'
+import clsx from 'clsx';
+import styles from './styles.module.css'
+import Link from '@docusaurus/Link';
 
 
-function DocItem({doc}) {
-    return (
-      <article className="margin-vert--lg">
-        <Link to={doc.permalink}>
-          <h3>{doc.title}</h3>
-        </Link>
-        {doc.description && <p>{doc.description}</p>}
-      </article>
-    );
-  }
-  
+function Tag({permalink, label}) {
+  return (
+    <Link
+      href={permalink}
+      className={clsx(
+        styles.tag,
+        styles.tagRegular,
+      )}>
+      {label}
+    </Link>
+  );
+}
 
 export default function CourseList(props) {
 
@@ -26,19 +30,62 @@ export default function CourseList(props) {
     const oneTag = props.tag ? allTags[props.tag] : Object.values(allTags)
         .flatMap(a => a)
         .filter(uniqueOnly)
+        
     const filter = props.filter ? '/'+props.filter+'/' : ''
     const location = useLocation().pathname;
 
     oneTag.sort((a, b) => a.order - b.order);
 
-    return (
-        <div class="bok-tag-list" key={props.tag}>   
-            {
-               oneTag
+	const data = oneTag
                 .filter(d => d.permalink.indexOf(filter) > -1) 
-                .filter(d => d.permalink.indexOf(location) == -1)
-                .map(d => <DocItem key={d} doc={d} />)
-            }
-        </div>
-    );
+                .filter(d => d.permalink.indexOf(location) == -1);
+                
+	const columns = [
+	  {
+	    Header: "Code",
+	    accessor: "frontMatter.course.code",
+	    className: "pester-data-table left",	  
+	  },
+	  {
+	    Header: "Title",
+	    accessor: "title",
+	    className: "pester-data-table left",
+	    Cell: ({ cell: { value }, row: { original } }) => (
+	      <a href={`${original.frontMatter.course.link}`} target="blank" rel="noreferrer noopener">
+	        {value}
+	      </a>
+	    ),
+	  },
+	  {
+	    Header: "Course Length",
+	    accessor: "frontMatter.course.length",
+	    className: "pester-data-table left"
+	  },
+	  {
+	    Header: "Tags",
+	    accessor: "frontMatter.tags",
+	    className: "pester-data-table",
+	    Cell: ({ cell: { value }, row: { original } }) => (
+	      <div>
+	      	{
+	      		original.frontMatter.tags
+	      			.filter(tag => tag.indexOf("(Role)") > -1)
+	      			.map(tag => tag.substring(0, tag.length - 7))
+	      			.map(tag => (<Tag label={tag} />))
+	      		
+	      	}
+	      </div>
+	    )
+	  },
+	  {
+	    Header: "Recommendation",
+	    accessor: "frontMatter.course.recommended",
+	    className: "pester-data-table left"
+	  }	  
+	];
+	
+	return (
+		<PesterDataTable columns={columns} data={data} />
+	)
+
 }
