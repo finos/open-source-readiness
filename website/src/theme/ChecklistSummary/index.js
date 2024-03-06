318538be0a893764905a42e0b9b8727ec7db9c54
Rob Moffat <rob@kite9.com>
2024-03-06 17:03:13 +0000

    Fixed minor bug in colours

--- website/src/theme/ChecklistSummary/index.js
+++ website/src/theme/ChecklistSummary/index.js
@@ -43,43 +43,45 @@ function Square({ name, level, row }) {
 	const allDocs = usePluginData('checklist-listing');
 	const complete = allChecklists(allDocs)
 	const docDetails = complete.filter(d => matchesTag(d.tags, name + " (OSMM)"))
+	const zeroPad = (num, places) => String(num).padStart(places, '0')
 
 	const total = docDetails.length
 	const relevantDocs = docDetails.filter(d => d.level == "" + level)
 	const count = relevantDocs.length
 	const opacity = Math.floor((count / total) * 128).toString(16);
-	const completeColour = `#${levels[level].color}${opacity}`
+	const completeColour = `#${levels[level].color}${zeroPad(opacity, 2)}`
+	console.log("COMplete colour:"+completeColour)
 
 	return (
-		<div className={styles.square} style={{ backgroundColor: completeColour, gridColumnStart: level+2, gridRowStart: row }}>
+		<div className={styles.square} style={{ backgroundColor: completeColour, gridColumnStart: level + 2, gridRowStart: row }}>
 			<h4 className={styles.name}><a href={levels[level].link} title={levels[level].description}>{levels[level].name}</a></h4>
 			<div className={styles.score}>{count}</div>
 			<ul className={styles.items}>
 				{(relevantDocs.length < 6) ? relevantDocs.map(d => <DetailRow key={d.slug + "_" + d.name} d={d} />) : relevantDocs.slice(0, 4).map(d => <DetailRow key={d.slug + "_" + d.name} d={d} />)}
-			{(relevantDocs.length > 5) ? <li><em>& {relevantDocs.length - 4} others</em></li> : ""}
+				{(relevantDocs.length > 5) ? <li><em>& {relevantDocs.length - 4} others</em></li> : ""}
 			</ul>
-			
+
 		</div>
 	)
 }
 
 function DoDocList({ row, uri, name, icon, description }) {
 	return (
-		<div className={styles.level} style={{gridColumnStart: 1, gridRowStart: row}}>
-				<h4 className={styles.name}><a href={uri}>{name}</a></h4>
-				<img className={styles.score} src={icon} />
-				<ul className={styles.items}><li>{description}</li></ul>
+		<div className={styles.level} style={{ gridColumnStart: 1, gridRowStart: row }}>
+			<h4 className={styles.name}><a href={uri}>{name}</a></h4>
+			<img className={styles.score} src={icon} />
+			<ul className={styles.items}><li>{description}</li></ul>
 		</div>
 	)
 }
 
-const tags= ["Level 5", "Level 4","Level 3","Level 2","Level 1"]
+const tags = ["Level 5", "Level 4", "Level 3", "Level 2", "Level 1"]
 
 function ChecklistSummaryInner() {
 	return (
 		<div id="checklist" className={styles.checklistOutline}>
-			<div className={styles.banner}>					
-				<h2>FINOS Open Source Readiness Maturity Checklist</h2>				
+			<div className={styles.banner}>
+				<h2>FINOS Open Source Readiness Maturity Checklist</h2>
 			</div>
 			<div className={styles.checklistSummary} id="checklist-summary-inner">
 				<DoDocList row={1} uri="/docs/bok/OSMM/Checklist#level-5" name="Level 5" description="Strategic Advantage" icon="/img/bok/maturity/strategy.png" />
@@ -87,11 +89,11 @@ function ChecklistSummaryInner() {
 				<DoDocList row={3} uri="/docs/bok/OSMM/Checklist#Level-3" name="Level 3" description="Contribution" icon="/img/bok/maturity/contributing.png" />
 				<DoDocList row={4} uri="/docs/bok/OSMM/Checklist#Level-2" name="Level 2" description="Compliant Usage" icon="/img/bok/maturity/compliance.png" />
 				<DoDocList row={5} uri="/docs/bok/OSMM/Checklist#Level-1" name="Level 1" description="Ad-Hoc Usage" icon="/img/bok/maturity/using.png" />
-			
+
 				{
-					tags.flatMap((t,j) => levels.map((_l, i) => <Square key={i+"_"+j} name={t} level={i} row={j+1}/>))
+					tags.flatMap((t, j) => levels.map((_l, i) => <Square key={i + "_" + j} name={t} level={i} row={j + 1} />))
 				}
-			
+
 			</div>
 		</div>
 	)
